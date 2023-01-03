88f6eb1626a535367e36407e371d8c44d02b1366
Svetlana <se55petrova@gmail.com>
2023-01-03 14:06:35 +0500

    Изменила функцию для получения случайного числа из заданного диапазона

--- js/main.js
+++ js/main.js
@@ -1,36 +1,12 @@
-function getRandomNumber(min, max) {
-  if (min < 0 || max < 0) {
-    console.log("Числа должны быть положительными");
-    return
-  }
-  if (typeof min == "string" || typeof max == "string") {
-    console.log('Введена строка');
-    return;
-  }
-  if (typeof min == 'undefined' && typeof max == 'undefined') {
-    let RandomNumber = Math.floor(Math.random()*1000+1); // Покажет от 0 до 1000 включительно
-    console.log(RandomNumber)
-    return RandomNumber;
-  }
-  if (typeof max == 'undefined') {
-    max = min;
-    min = 0;
-  }
-  if (min > max) {
-    min = Math.floor(min);
-    max = Math.ceil(max);
-    let RandomNumber = Math.floor(Math.random()*(min-max+1))+max;
-    console.log(RandomNumber);
-    return RandomNumber;
-  }
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let RandomNumber = Math.floor(Math.random()*(max-min+1))+min;
-    console.log(RandomNumber);
-    return RandomNumber;
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-getRandomNumber();
+getRandomPositiveInteger(1, 7)
 
 function checkStringLength(comment, maxStringLength) {
   if (comment.length <= maxStringLength) {
